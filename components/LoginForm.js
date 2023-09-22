5a92785f088491994d86ccdc8ebfbe9188561e6e
owusuvictorjnr <owusuvictor80@gmail.com>
2023-09-22 13:19:39 +0000

    inital commit

---
+++ components/LoginForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoginForm = () => {
+  return (
+    <div className="grid place-items-center h-screen">
+      <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
+        <h1 className="text-l font-bold my-4 capitalize">enter details</h1>
+      </div>
+    </div>
+  )
+}
+
+export default LoginForm
