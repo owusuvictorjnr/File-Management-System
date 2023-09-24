fdbf2bd431774614364b5e040b68112b4f37b94d
owusuvictorjnr <owusuvictor80@gmail.com>
2023-09-24 11:02:00 +0000

    register page redirection

--- app/register/page.js
+++ app/register/page.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import RegisterForm from '@/components/RegisterForm'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
+import { authOptions } from '../api/auth/[...nextauth]/route'
 
 export default function Register() {
+  const session = getServerSession(authOptions)
+
+  if (session) {
+    redirect('/dashboard')
+  }
   return <RegisterForm />
 }
