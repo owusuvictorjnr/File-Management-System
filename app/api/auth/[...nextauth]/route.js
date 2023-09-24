daeb2f95f360f0613a12f4a13dc6326f62022819
owusuvictorjnr <owusuvictor80@gmail.com>
2023-09-24 11:01:21 +0000

    auth user password to see if it matches

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,33 @@
+import User from '@/models/user'
 import NextAuth from 'next-auth/next'
+import bcrypt from 'bcryptjs'
 import CredentialsProvider from 'next-auth/providers/credentials'
 
-const authOptions = {
+export const authOptions = {
   providers: [
     CredentialsProvider({
       name: 'credentials',
       credentials: {},
 
       async authorize(credentials) {
-        const user = { id: '1' }
-        return user
+        const { email, password } = credentials
+
+        try {
+          const user = await User.findOne({ email })
+
+          if (!user) {
+            return null
+          }
+
+          const passwordsMatch = await bcrypt.compare(password, user.password)
+
+          if (!passwordsMatch) {
+            return null
+          }
+          return user
+        } catch (error) {
+          console.log('Error', error)
+        }
       },
     }),
   ],
