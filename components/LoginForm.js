a241306fc148e9af947ac6de570902c29ffbfbe6
owusuvictorjnr <owusuvictor80@gmail.com>
2023-09-24 11:02:39 +0000

    Login validation

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -2,21 +2,58 @@
 
 import Link from 'next/link'
 import React, { useState } from 'react'
+import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 
 const LoginForm = () => {
+  const [email, setEmail] = useState('')
+
+  const [password, setPassword] = useState('')
+
   const [error, setError] = useState('')
+
+  const router = useRouter()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const res = await signIn('credentials', {
+        email,
+        password,
+        redirect: false,
+      })
+
+      if (res.error) {
+        setError('Invalid credentials')
+        return
+      }
+
+      router.replace('dashboard')
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <div className="grid place-items-center h-screen">
-      <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
+      <div className="shadow-lg p-5 rounded-lg border-t-4 border-yellow-400">
         <h1 className="text-l font-bold my-4 capitalize">sign in</h1>
 
-        <form className="flex flex-col gap-3">
-          <input type="text" placeholder="Email" />
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="Email"
+          />
 
-          <input type="text" placeholder="Password" />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="text"
+            placeholder="Password"
+          />
 
-          <button className="bg-green-600 text-white capitalize font-bold px-6 py-2">
+          <button className="bg-green-600 hover:bg-zinc-200 duration-500 text-white capitalize font-bold px-6 py-2">
             login
           </button>
 
