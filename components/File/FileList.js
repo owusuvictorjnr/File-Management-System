c4668183ea645fbbcd6e49c36d74a2784f38ef0a
owusuvictorjnr <owusuvictor80@gmail.com>
2023-09-28 22:14:50 +0000

    add action

--- components/File/FileList.js
+++ components/File/FileList.js
@@ -33,27 +33,15 @@ function FileList() {
   ]
 
   return (
-    <div
-      className="bg-white mt-5 p-5 px-10 mx-5
-    rounded-lg"
-    >
+    <div className="bg-white mt-5 p-5 px-10 mx-5 rounded-lg">
       <h2 className="text-[1rem] font-bold">Recent Files</h2>
-      <div
-        className="grid grid-cols-3
-        md:grid-cols-3
-        text-[.8rem]
-        font-semibold
-        border-b-[1px]
-        pb-2 mt-3
-        border-gray-300
-         text-gray-400"
-      >
+      <div className="grid grid-cols-4 md:grid-cols-3 text-[.8rem] font-semibold border-b-[1px] pb-2 mt-3 border-gray-300 text-gray-400">
         <h2>Name</h2>
-        {/* <div className="grid grid-cols-3 md:gap-[15rem]"> */}
-        <h2>Modified</h2>
-        <h2>Size</h2>
-        {/* <h2></h2> */}
-        {/* </div> */}
+        <div className="grid grid-cols-3 gap-[5rem] md:gap-[9rem] lg:gap-[17rem]">
+          <h2>Modified</h2>
+          <h2>Size</h2>
+          <h2>action</h2>
+        </div>
       </div>
       <div className="text-sm flex flex-col">
         {fileList &&
