0e087876f11d83ffc212e92d79e4ce9d3f7e6166
owusuvictorjnr <owusuvictor80@gmail.com>
2023-09-29 07:20:22 +0000

    use contex and use effect

--- components/Toast.js
+++ components/Toast.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import { ShowToastContext } from '@/context/ShowToastContext'
+import React, { useContext, useEffect } from 'react'
 
-const Toast = () => {
+const Toast = ({ msg }) => {
+  const { showToastMsg, setShowToastMsg } = useContext(ShowToastContext)
+
+  useEffect(() => {
+    setInterval(() => {
+      setShowToastMsg(null)
+    }, 3000)
+  }, [showToastMsg])
   return (
     <div className="toast toast-top toast-end">
       <div className="alert alert-success">
-        <span>Message sent successfully.</span>
+        <span>{msg}.</span>
       </div>
     </div>
   )
