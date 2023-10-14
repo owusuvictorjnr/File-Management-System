ff2fd7d1dcd79f4af3046c30dfb510a647776a6b
owusuvictorjnr <owusuvictor80@gmail.com>
2023-10-14 17:49:47 +0000

    change path

--- components/Folder/FolderList.js
+++ components/Folder/FolderList.js
@@ -1,86 +1,56 @@
 import React, { useState } from 'react'
 import FolderItem from './FolderItem'
 import { useRouter } from 'next/navigation'
+
 // import FolderItemSmall from './FolderItemSmall'
 
-function FolderList({ isBig = true }) {
+function FolderList({}) {
   const [activeFolder, setActiveFolder] = useState()
   const router = useRouter()
 
   const folderList = [
     {
       id: 1,
-      name: 'Folder 1',
+      name: 'CS & CE',
     },
     {
       id: 2,
-      name: 'Folder 2',
+      name: 'B M E',
     },
     {
       id: 3,
-      name: 'Folder 3',
+      name: 'Nursing',
     },
     {
       id: 4,
-      name: 'Folder 4',
+      name: 'ECE',
     },
     {
       id: 5,
-      name: 'Folder 4',
+      name: 'Oil & Gas',
     },
   ]
 
-  const onFolderClick = (index, item) => {
+  const onFolderClick = (index) => {
     setActiveFolder(index)
-    router.push({
-      pathname: '/folder/' + item.id,
-      query: {
-        name: item.name,
-        id: item.id,
-      },
-    })
+    router.push('/department/name')
   }
   return (
-    <div
-      className="p-5 mt-5 
-    bg-white rounded-lg"
-    >
-      {isBig ? (
-        <h2
-          className="text-17px] 
-        font-bold 
-        items-center"
-        >
-          Recent Folders
-          <span
-            className="float-right
-        text-blue-400 font-normal
-        text-[13px]"
-          >
-            View All
-          </span>
-        </h2>
-      ) : null}
-      {isBig ? (
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mt-3 gap-4">
-          {folderList.map((item, index) => (
-            <div key={index} onClick={() => onFolderClick(index, item)}>
-              <FolderItem folder={item} />
-            </div>
-          ))}
-        </div>
-      ) : (
-        <div
-          className=" 
-      "
-        >
-          {folderList.map((item, index) => (
-            <div key={index} onClick={() => onFolderClick(index, item)}>
-              {/* <FolderItemSmall folder={item} /> */}
-            </div>
-          ))}
-        </div>
-      )}
+    <div className="p-5 mt-5 bg-white rounded-lg">
+      <h2 className="text-[2rem] font-bold items-center">
+        Departments
+        {/* <span className="float-right text-blue-400 font-normal text-[13px]">
+          View All
+        </span> */}
+      </h2>
+
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mt-3 gap-4">
+        {folderList.map((item, index) => (
+          <div key={index} onClick={() => onFolderClick(item.id)}>
+            <FolderItem folder={item} activeFolder={activeFolder == index} />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
