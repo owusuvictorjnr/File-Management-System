68ed004536134d9330d2bdcbb64d2332ce4147c2
owusuvictorjnr <owusuvictor80@gmail.com>
2023-10-14 17:44:21 +0000

    upload file

---
+++ app/department/upload-file/page.js
@@ -0,0 +1,12 @@
+import UploadFile from '@/components/UploadFile'
+import React from 'react'
+
+const UploadFiles = () => {
+  return (
+    <div>
+      <UploadFile />
+    </div>
+  )
+}
+
+export default UploadFiles
