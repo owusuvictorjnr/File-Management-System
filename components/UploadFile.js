cc89597d9653df370e1d2175f5ccdb11d68ff7c3
owusuvictorjnr <owusuvictor80@gmail.com>
2023-10-16 17:34:59 +0000

    upload file db

--- components/UploadFile.js
+++ components/UploadFile.js
@@ -1,38 +1,122 @@
 'use client'
 
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { useRouter } from 'next/router'
+import { useSession } from 'next-auth/react'
 
 const UploadFile = () => {
+  const [title, setTitle] = useState('')
+  const [name, setStudentName] = useState('')
+  const [department, setDepartment] = useState('')
+  const [indexNumber, setIndexNumber] = useState('')
+  const [supervisor, setSupervisor] = useState('')
+  const [deptHead, setDeptHead] = useState('')
+  const [calendar, setCalendar] = useState('')
+  const [uploadFile, setUploadFile] = useState('')
+  const [error, setError] = useState('')
+  // const [success, setSuccess] = useState('')
+
   const handleSubmit = async (e) => {
     e.preventDefault()
+    if (
+      !title ||
+      !name ||
+      !department ||
+      !indexNumber ||
+      !supervisor ||
+      !deptHead ||
+      !calendar ||
+      !uploadFile
+    ) {
+      setError('All fields are required')
+      return
+    }
 
     try {
+      const res = await fetch('api/fileUploadInfo', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title,
+          name,
+          department,
+          indexNumber,
+          supervisor,
+          deptHead,
+          calendar,
+          uploadFile,
+        }),
+      })
+
       if (res.ok) {
         const form = e.target
         form.reset()
         router.push('/')
       } else {
-        console.log('user registration failed')
+        console.log('File upload failed')
       }
     } catch (error) {
-      console.log('Error during registration', error)
+      console.log('Error during file upload', error)
     }
   }
+
   return (
     <div className="grid place-items-center h-screen">
       <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-        <input type="text" placeholder="Project Title" />
-        <input type="text" placeholder="Student Name" />
-        <input type="text" placeholder="Department" />
-        <input type="text" placeholder="Indx Number" />
-        <input type="text" placeholder="Project Supervisor" />
-        <input type="text" placeholder="Head of Department" />
-        <input type="date" placeholder="Date of Submission" />
-        <input type="file" placeholder="Choose File" />
-        <button className="bg-green-600 hover:bg-zinc-200/40 hover:text-orange-600 duration-700 text-white capitalize font-bold px-6 py-2">
+        <input
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="Project Title"
+        />
+        <input
+          onChange={(e) => setStudentName(e.target.value)}
+          type="text"
+          placeholder="Student Name"
+        />
+        <input
+          onChange={(e) => setDepartment(e.target.value)}
+          type="text"
+          placeholder="Department"
+        />
+        <input
+          onChange={(e) => setIndexNumber(e.target.value)}
+          type="text"
+          placeholder="Index Number"
+        />
+        <input
+          onChange={(e) => setSupervisor(e.target.value)}
+          type="text"
+          placeholder="Project Supervisor"
+        />
+        <input
+          onChange={(e) => setDeptHead(e.target.value)}
+          type="text"
+          placeholder="Head of Department"
+        />
+        <input
+          onChange={(e) => setCalendar(e.target.value)}
+          type="date"
+          placeholder="Date of Submission"
+        />
+        <input
+          onChange={(e) => setUploadFile(e.target.value)}
+          type="file"
+          placeholder="Choose File"
+        />
+        <button
+          text="submit"
+          className="bg-green-600 hover:bg-zinc-200/40 hover:text-orange-600 duration-700 text-white capitalize font-bold px-6 py-2"
+        >
           upload
         </button>
+
+        {error && (
+          <div className="bg-red-500 capitalize text-white w-fit text-sm py-1 px-3 rounded-md">
+            {error}
+          </div>
+        )}
       </form>
     </div>
   )
