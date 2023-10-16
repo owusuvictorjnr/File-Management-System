5778643fa26fd542e3d1fe37016f61c55222f15a
owusuvictorjnr <owusuvictor80@gmail.com>
2023-10-16 17:33:53 +0000

    view file page

---
+++ app/view-file/page.js
@@ -0,0 +1,14 @@
+import ViewFile from '@/components/ViewFile'
+import React from 'react'
+
+const viewFile = () => {
+  return (
+    <>
+      <div>
+        <ViewFile />
+      </div>
+    </>
+  )
+}
+
+export default viewFile
