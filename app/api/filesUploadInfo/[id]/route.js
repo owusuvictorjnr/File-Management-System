9d7716c892e5a786fc3232fc2cb71f8fb8a20c2f
owusuvictorjnr <owusuvictor80@gmail.com>
2023-10-17 09:53:47 +0000

    update and fetching by id

---
+++ app/api/filesUploadInfo/[id]/route.js
@@ -0,0 +1,25 @@
+import FileUploadInfo from '@/models/fileUploadInfo'
+import { connectMongoDB } from '@/utils/mongodb'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function PUT(req, { params }) {
+  const { id } = params
+  const { newProjecTitle: projecTitle, newStudentName: studentName } =
+    await req.json()
+
+  await connectMongoDB()
+
+  await FileUploadInfo.findByIdAndUpdate(id, { projecTitle, studentName })
+
+  return NextRequest.json({ message: 'File updated successfully' })
+}
+
+// fetching one file by its id
+export async function GET(req, { params }) {
+  const { id } = params
+
+  await connectMongoDB()
+  const fileUploadInfo = await FileUploadInfo.findOne({ _id: id })
+
+  return NextResponse.json({ fileUploadInfo }, { status: 200 })
+}
