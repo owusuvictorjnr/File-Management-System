a450c4ad6890d593afd96a9c5da87dd32d656e87
owusuvictorjnr <owusuvictor80@gmail.com>
2023-10-17 08:42:46 +0000

    file upload frontend  page

--- app/department/upload-file/page.js
+++ app/department/upload-file/page.js
@@ -1,12 +1,62 @@
-import UploadFile from '@/components/UploadFile'
-import React from 'react'
+'use client'
 
-const UploadFiles = () => {
+import React, { useContext, useState } from 'react'
+
+// projecTitle,
+//     studentName,
+//     department,
+//     indexNumber,
+//     deptHead,
+//     supervisor,
+
+const UploadFile = () => {
   return (
-    <div>
-      <UploadFile />
+    <div className="grid place-items-center h-screen">
+      <form className="flex flex-col gap-3">
+        <input
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="Project Title"
+        />
+        <input
+          onChange={(e) => setStudentName(e.target.value)}
+          type="text"
+          placeholder="Student Name"
+        />
+
+        <input
+          onChange={(e) => setStudentName(e.target.value)}
+          type="text"
+          placeholder="Department Name"
+        />
+
+        <input
+          onChange={(e) => setStudentName(e.target.value)}
+          type="text"
+          placeholder="Index Number"
+        />
+
+        <input
+          onChange={(e) => setStudentName(e.target.value)}
+          type="text"
+          placeholder="HOD"
+        />
+
+        <input
+          onChange={(e) => setStudentName(e.target.value)}
+          type="text"
+          placeholder="Supervisor"
+        />
+
+        <button
+          text="submit"
+          className="bg-green-600 hover:bg-zinc-200/40 hover:text-orange-600 duration-700 text-white capitalize font-bold px-6 py-2"
+        >
+          upload
+        </button>
+      </form>
     </div>
   )
 }
 
-export default UploadFiles
+export default UploadFile
