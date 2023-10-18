5a9d62cb2dd75d2151992d16ea379aa647fb0087
owusuvictorjnr <owusuvictor80@gmail.com>
2023-10-18 06:38:02 +0000

    add calendar and file inputes

--- components/UploadFile.js
+++ components/UploadFile.js
@@ -1,122 +1,129 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
 import React, { useContext, useState } from 'react'
-import { useRouter } from 'next/router'
-import { useSession } from 'next-auth/react'
 
 const UploadFile = () => {
-  const [title, setTitle] = useState('')
-  const [name, setStudentName] = useState('')
+  const [projecTitle, setProjecTitle] = useState('')
+  const [studentName, setStudentName] = useState('')
   const [department, setDepartment] = useState('')
   const [indexNumber, setIndexNumber] = useState('')
-  const [supervisor, setSupervisor] = useState('')
   const [deptHead, setDeptHead] = useState('')
+  const [supervisor, setSupervisor] = useState('')
   const [calendar, setCalendar] = useState('')
-  const [uploadFile, setUploadFile] = useState('')
-  const [error, setError] = useState('')
-  // const [success, setSuccess] = useState('')
+  const [file, setFile] = useState('')
+
+  const router = useRouter()
 
   const handleSubmit = async (e) => {
     e.preventDefault()
+
     if (
-      !title ||
-      !name ||
+      !projecTitle ||
+      !studentName ||
       !department ||
       !indexNumber ||
-      !supervisor ||
       !deptHead ||
+      !supervisor ||
       !calendar ||
-      !uploadFile
+      !file
     ) {
-      setError('All fields are required')
+      alert('All fields are required')
       return
     }
 
     try {
-      const res = await fetch('api/fileUploadInfo', {
+      const res = await fetch('http://localhost:3000/api/filesUploadInfo', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          title,
-          name,
+          projecTitle,
+          studentName,
           department,
           indexNumber,
-          supervisor,
           deptHead,
-          calendar,
-          uploadFile,
+          supervisor,
         }),
       })
 
       if (res.ok) {
         const form = e.target
         form.reset()
-        router.push('/')
+        router.push('/dashboard')
       } else {
-        console.log('File upload failed')
+        console.log('user registration failed')
       }
     } catch (error) {
-      console.log('Error during file upload', error)
+      console.log('Error during registration', error)
     }
   }
+
+  //   console.log('Title: ', projecTitle, 'Student name:', studentName)
 
   return (
     <div className="grid place-items-center h-screen">
       <form onSubmit={handleSubmit} className="flex flex-col gap-3">
         <input
-          onChange={(e) => setTitle(e.target.value)}
+          onChange={(e) => setProjecTitle(e.target.value)}
+          value={projecTitle}
           type="text"
           placeholder="Project Title"
         />
         <input
           onChange={(e) => setStudentName(e.target.value)}
+          value={studentName}
           type="text"
           placeholder="Student Name"
         />
+
         <input
           onChange={(e) => setDepartment(e.target.value)}
+          value={department}
           type="text"
-          placeholder="Department"
+          placeholder="Department Name"
         />
+
         <input
           onChange={(e) => setIndexNumber(e.target.value)}
+          value={indexNumber}
           type="text"
           placeholder="Index Number"
         />
-        <input
-          onChange={(e) => setSupervisor(e.target.value)}
-          type="text"
-          placeholder="Project Supervisor"
-        />
+
         <input
           onChange={(e) => setDeptHead(e.target.value)}
+          value={deptHead}
           type="text"
-          placeholder="Head of Department"
+          placeholder="HOD"
         />
+
+        <input
+          onChange={(e) => setSupervisor(e.target.value)}
+          value={supervisor}
+          type="text"
+          placeholder="Supervisor"
+        />
+
         <input
           onChange={(e) => setCalendar(e.target.value)}
+          value={calendar}
           type="date"
-          placeholder="Date of Submission"
         />
+
         <input
-          onChange={(e) => setUploadFile(e.target.value)}
+          onChange={(e) => setFile(e.target.value)}
+          value={file}
           type="file"
-          placeholder="Choose File"
         />
+
         <button
           text="submit"
           className="bg-green-600 hover:bg-zinc-200/40 hover:text-orange-600 duration-700 text-white capitalize font-bold px-6 py-2"
         >
           upload
         </button>
-
-        {error && (
-          <div className="bg-red-500 capitalize text-white w-fit text-sm py-1 px-3 rounded-md">
-            {error}
-          </div>
-        )}
       </form>
     </div>
   )
