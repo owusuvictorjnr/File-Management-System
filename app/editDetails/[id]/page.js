cd5eb1704080ceefe0868eb7f27bf9efd8634b8a
owusuvictorjnr <owusuvictor80@gmail.com>
2023-10-18 15:51:23 +0000

    edit button

--- app/editDetails/[id]/page.js
+++ app/editDetails/[id]/page.js
@@ -1,7 +1,39 @@
+import EditDetailsForm from '@/components/EditDetailsForm'
 import React from 'react'
 
-const EditDetails = () => {
-  return <div>EditDetails</div>
+const getDetailsById = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/filesUploadInfo/${id}`, {
+      cache: 'no-store',
+    })
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch details')
+    }
+
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const EditDetails = async ({ params }) => {
+  const { id } = params
+
+  const { fileUploadInfo } = await getDetailsById(id)
+
+  const { projecTitle, studentName } = fileUploadInfo
+
+  // console.log('id:', id)
+  return (
+    <div>
+      <EditDetailsForm
+        id={id}
+        projecTitle={projecTitle}
+        studentName={studentName}
+      />
+    </div>
+  )
 }
 
 export default EditDetails
