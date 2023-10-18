a39512b071c41ab1bfabacfcf45197f783971a31
owusuvictorjnr <owusuvictor80@gmail.com>
2023-10-18 06:36:45 +0000

    Remove button

---
+++ components/RemoveBtn.js
@@ -0,0 +1,15 @@
+'use client'
+import Link from 'next/link'
+import React from 'react'
+
+const RemoveBtn = () => {
+  return (
+    <div>
+      <Link href={'/editFile'}>
+        
+      </Link>
+    </div>
+  )
+}
+
+export default RemoveBtn
