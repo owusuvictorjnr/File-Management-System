cb8d1a93c4c45441433c17292600414526108304
owusuvictorjnr <owusuvictor80@gmail.com>
2023-10-20 06:21:32 +0000

    drag and drup input

--- components/UploadFile.js
+++ components/UploadFile.js
@@ -2,6 +2,7 @@
 
 import { useRouter } from 'next/navigation'
 import React, { useContext, useState } from 'react'
+import UploadFileModal from './File/UploadFileModal'
 
 const UploadFile = () => {
   const [projecTitle, setProjecTitle] = useState('')
@@ -105,11 +106,46 @@ const UploadFile = () => {
           placeholder="Supervisor"
         />
 
-        <input
-          onChange={(e) => setFile(e.target.value)}
-          value={file}
-          type="file"
-        />
+        <div
+          className="w-full items-center 
+        flex flex-col justify-center gap-3"
+        >
+          <div className="flex items-center justify-center w-full">
+            <label className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                <svg
+                  className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 16"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                  />
+                </svg>
+                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                  <span className="font-semibold">Click to upload</span> or drag
+                  and drop
+                </p>
+                <p className="text-xs text-gray-500 dark:text-gray-400">
+                  SVG, PNG, JPG or GIF (MAX. 800x400px)
+                </p>
+              </div>
+              <input
+                // id="dropzone-file"
+                type="file"
+                value={file}
+                // className="hidden"
+                onChange={(e) => setFile(e.target.value)}
+              />
+            </label>
+          </div>
+        </div>
 
         <button
           text="submit"
